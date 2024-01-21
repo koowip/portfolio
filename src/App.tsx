a59996f98a6e6155e2b4b0dc405cfad7e2196267
Patrick White <patrickwhite.swe@gmail.com>
2024-01-21 12:27:26 -0700

    Header icon/link working, further style needed. Will probably change to a more muted white.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import '../src/styles/App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import '../styles/Header.css';
+
 export default function Header () {
 
   return (
     <>
-    <h1>Header Component</h1>
+    <div className="header-box">
+      <h1>Patrick</h1>
+      <div className="links">
+        <a href="https://github.com/koowip">
+          <FaGithub size={25}/>
+        </a>
+        <a href="https://www.linkedin.com/in/patrickdeanwhiteswe/">
+          <FaLinkedin size={25}/>
+        </a>
+      </div>
+    </div>
     </>
   )
 }
